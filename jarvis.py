975549551ee55e1135e5ec522f86a147d15bbc02
SaiSri Angajala <63279776+SaiSri44@users.noreply.github.com>
2021-04-12 13:17:38 +0530

    added youtube search and youtube automation , closing of system apps abilites

--- jarvis.py
+++ jarvis.py
@@ -16,6 +16,7 @@ import PyPDF2
 import pytube
 import psutil
 from bs4 import BeautifulSoup
+import keyboard
 
 engine = pyttsx3.init()
 voices = engine.getProperty("voices")
@@ -30,9 +31,9 @@ engine.setProperty("voice", voices[3].id)
 #
 # Above code gives the voices, voices[0] is david,and voices[1] is zara,we can switch between the voices by changing the indexes
 # we can install more voices in our in systema also, david and zara come by dafault
-#i had added the mark voice and david Mvoices  extra to the system 
-#now the voice[0] is david voice[1] is mark and voice[2] is davidM and voices[3] is zira 
- 
+# i had added the mark voice and david Mvoices  extra to the system
+# now the voice[0] is david voice[1] is mark and voice[2] is davidM and voices[3] is zira
+
 # controlling the speech rate,this can be done by using one line
 engine.setProperty('rate', 180)
 # normal values are between 180-200 if we decrease then it will speak slow and if increase it will speak fast
@@ -63,6 +64,9 @@ class system_apps():
         path = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Accessories\\Notepad"
         os.startfile(path)
 
+    def close_notepad(self):
+        os.system("TASKKILL /f /im notepad.exe") 
+
     def open_google(self):
         path = "C:\\ProgramData\Microsoft\\Windows\\Start Menu\\Programs\\Google Chrome"
         self.speak("what do you want to search on google sir")
@@ -70,10 +74,14 @@ class system_apps():
         self.speak("opening google")
         webbrowser.open(search)
 
+    def close_chrome(self) :
+        os.system("TASKKILL /f /im chrome.exe")    
+
     def open_command_prompt(self):
-        self.speak("opening command prompt")
         os.system("start cmd")
 
+    def close_command_prompt(self) :
+        os.system("TASKKILL /f /im cmd.exe")
 
 class jarvis_abilites1():
     def speak(self, audio):
@@ -86,7 +94,7 @@ class jarvis_abilites1():
         name = self.take_command().lower()
         self.speak("Hang on for few seconds sir , I am taking the screenshot")
         time.sleep(3)
-        img = pyautogui.screenshot()
+        img = pyautogui.screenshot() 
         img.save(f"{name}.jpg")
         self.speak(
             "ok sir i am done with taking screenshot, it is saved in our current folder, i am ready for another command sir")
@@ -152,7 +160,7 @@ class jarvis_abilites1():
         print("searching wikipedia.....")
         self.speak("searching wikipedia")
         query = query.replace("wikipedia", "")
-        results = wikipedia.summary(query, sentences=3)
+        results = wikipedia.summary(query, sentences=3) 
         self.speak("according to wikipedia")
         self.speak(results)
 
@@ -167,7 +175,7 @@ class jarvis_abilites1():
             self.speak(f"current {weather} is {temperature[:2]} degrees")
         else:
             return temperature
-
+    
 
 class jarvis_abilites2():
     def speak(self, audio):
@@ -193,6 +201,8 @@ class jarvis_abilites2():
             elif percentage < 20:
                 self.speak(
                     "sir , our batttery is very low ,immediately plug in the adapter")
+    
+  
 
 
 class jarvis_code(social_media, system_apps, jarvis_abilites1, jarvis_abilites2):
@@ -244,7 +254,7 @@ class jarvis_code(social_media, system_apps, jarvis_abilites1, jarvis_abilites2)
                 self.open_notepad()
 
             # openiong the google chrome
-            elif "open google" in query:
+            elif "open google" in query or "open chrome" in query:
                 self.open_google()
 
             # opening command prompt
@@ -309,7 +319,7 @@ class jarvis_code(social_media, system_apps, jarvis_abilites1, jarvis_abilites2)
                 break
 
             # terminating the programm execution
-            elif "go offline" in query:
+            elif "go offline" in query or "offline" in query:
                 self.speak("Thank you sir for using me,have a nice day")
                 sys.exit()
 
@@ -352,14 +362,67 @@ class jarvis_code(social_media, system_apps, jarvis_abilites1, jarvis_abilites2)
             # getting battery percentage
             elif "how much power left" in query or "how much battery we have" in query:
                 self.battery_percentage()
-             
-             #raising the volume
-            elif "raise volume"  in query :
+
+             # raising the volume
+            elif "raise volume" in query:
                 pyautogui.press("volumeup")
-            elif "decrease volume" in query :
-                pyautogui.press("volumedown") 
-            elif "mute volume" in query or "mute" in query : 
-                pyautogui.press("volumemute") 
+            elif "decrease volume" in query:
+                pyautogui.press("volumedown")
+            elif "mute volume" in query or "mute" in query:
+                pyautogui.press("volumemute")
+            # elif "set alarm" in query or "alarm" in query :
+            #     self.speak("sir , please enter the time for setting alarm " )
+            #     alarm = input("Enter the time")
+            #     self.speak("sir , alarm set successfully ")
+            # now = datetime.datetime.now()
+            # current_time = now.strftime("%H:%M")
+
+            # searching for anything in the youtube
+            elif "search in youtube" in query or "youtube search" in query:
+                self.speak("what do you wanna search in youtube sir")
+                youtube_search = self.take_command()
+                url = "https://www.youtube.com/results?search_query=" + youtube_search
+                self.speak("searching for the query in youtube")
+                webbrowser.open(url)
+                self.speak("sir , here is what i found in youtube")
+
+              #closing the notepad  
+            elif "close notepad" in query:
+                self.speak("closing notepad") 
+                self.close_notepad()
+
+            #  closing the google or chrome     
+            elif "close chrome" in query or "close google" in query :
+                self.speak("closing the chrome") 
+                self.close_chrome()
+
+            # closing the command prompt 
+            elif  "close command prompt" in query :
+                self.speak("closing the command prompt")    
+                self.close_command_prompt() 
+
+            #youtube automation
+            elif "full screen" in query :
+                 keyboard.press("f")
+            elif "stop" in query or "pause" in query:
+                keyboard.press("space bar")    
+            elif "play again" in query :
+                keyboard.press("0")
+            elif "mute" in query :
+                keyboard.press("m")    
+            elif "forward" in query or "skip" in query:
+                keyboard.press("l")
+            elif "backward" in query or "back" in query:
+                keyboard.press("j") 
+            elif "fill mode" in query :
+                keyboard.press("t")    
+
+            elif "repeat my words" in query :
+                self.speak("sir , i am listening speak now ")        
+                spoken_sentence = self.take_command()
+                self.speak(f"sir , you have spoken these ,  {spoken_sentence}")  
+
+
 
 jarvis = jarvis_code()
 # jarvis.speak("hello sir how can i help you")
